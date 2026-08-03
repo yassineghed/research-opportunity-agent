5fc5dcb377fa412d4eb55b8e5b564c92685473ee
yassineghed <114400953+yassineghed@users.noreply.github.com>
2026-08-03 23:31:20 +0100

    Refactor test_cordis_api.py: update API request handling and improve output logging

--- notebooks/testing/test_cordis_api.py
+++ notebooks/testing/test_cordis_api.py
@@ -1,4 +1,4 @@
-import sys
+"""import sys
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
@@ -18,4 +18,21 @@ params = {
 response = requests.get(url, params=params)
 
 print(response.status_code)
-print(response.text[:1000])
+print(response.text[:1000])"""
+
+import requests
+
+url = "https://cordis.europa.eu/api/dataextractions/getExtraction"
+
+params = {
+    "query": "artificial intelligence",
+    "key": "620a8386-b3eb-49a6-a950-9b05a8e4fc38",
+    "outputFormat": "json",
+    "archived": "false"
+}
+
+response = requests.get(url, params=params)
+
+print(response.url)
+print(response.status_code)
+print(response.text)
